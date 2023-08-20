7416a7891eff3c8264dca85893f9bcdccf0c6f11
joao vieira <john.collins.vieira@gmail.com>
2023-08-19 20:55:33 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      
+      if (array[j] === complement) return true;
+    }
+  }
+  
+  return false;
 }
+   
 
 /* 
   Write the Big O time complexity of your function here
@@ -9,6 +19,7 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
+
 
 /*
   Add written explanation of your solution here
